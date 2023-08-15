463fe9d2ac71036ffa58b0cf8a517eb1f5d2f2ac
lesponns <adamadakadam1903@gmail.com>
2023-08-15 21:17:30 +0300

    Add project files.

---
+++ SmartPro_35[15-08-23]/Hesaplama.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_35_15_08_23_
+{
+    internal class Hesaplama
+    {
+        public void Hesapla(int n1,int n2,string IslemTuru)
+        {
+            
+            int sonuc = 0;
+            string format = "{0} {1} {2} = {3}";
+            
+            switch (IslemTuru) 
+            {
+                case "+":
+                    sonuc = n1 + n2;
+                    break;
+                case "-":
+                    sonuc = n1 - n2;
+                    break;
+                case "*":
+                    sonuc = n1 * n2;
+                    break;
+                case "/":
+                    sonuc = n1 / n2;
+                    break;
+            }
+            Console.Write(format,n1,n2,sonuc);
+        }
+        public void Hesapla(int n1,int n2)
+        {
+            int last = n1 + n2;
+            Console.Write("İşlem seçmediğiniz için Toplanacak;\nToplama: {0} + {1} = {2}", n1, n2, last);
+        }
+    }
+}
---
+++ SmartPro_35[15-08-23]/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_35_15_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesaplama hesaplama = new Hesaplama();
+
+            Console.WriteLine("Sayıları giriniz.");
+            Console.Write("1. Sayı: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("2. Sayı: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nYapmak istediğiniz işlemi seçiniz.\n");
+            Console.Write("\nToplama +\nÇıkarma -\nÇarpma *\nBölme /\n");
+            string secim = Console.ReadLine();
+
+            hesaplama.Hesapla(n1, n2,secim);
+            if (string.IsNullOrEmpty(secim))
+            {
+                hesaplama.Hesapla(n1, n2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
